5cc4af23d9c4dccfb6948bb93ecdbaab9b2e137a
polymahh <otman.elkantaoui@gmail.com>
2024-01-14 10:30:58 +0100

    query stale time 60min

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -14,7 +14,7 @@ export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
           queries: {
             // With SSR, we usually want to set some default staleTime
             // above 0 to avoid refetching immediately on the client
-            staleTime: 60 * 1000,
+            staleTime: 60 * 60 * 1000,
             refetchOnWindowFocus: false,
           },
         },
